afdd8057e871bfc7e7cc8b8458016597642bcb76
viethung7899 <viethung07081999@gmail.com>
2021-09-02 21:37:09 -0700

    Remove old pendulum project

--- chaos-pendulum/README.md
+++
@@ -1,3 +0,0 @@
-# Chaos pendulum
-
-A simutation of the chaotic motion for n-pendulum system under gravitational field
--- chaos-pendulum/src/utils/angle.ts
+++
@@ -1,7 +0,0 @@
-export const toRadians = (degrees: number) => {
-  return (degrees / 180) * Math.PI;
-};
-
-export const toDegrees = (radians: number) => {
-  return (radians * 180) / Math.PI;
-};
